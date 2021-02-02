b48781ed190c84e8f0d8a5bd84c0f5c691bbf252
NahidaM <nahidanm@code.edu.az>
2021-02-02 20:32:35 +0400

    Middle nav done for desktop

--- Final Project/js/script.js
+++ Final Project/js/script.js
@@ -12,18 +12,5 @@ $(document).ready(function () {
     $(".mobile-navbar").addClass("active");
   });
 
-  // $(document).on("click", ".mobile-navbar ul li a", function () {
-  //   if ($(this).children("i").hasClass("fa-caret-right")) {
-  //     $(this)
-  //       .children("i")
-  //       .removeClass("fa-caret-right")
-  //       .addClass("fa-sort-down");
-  //   } else {
-  //     $(this)
-  //       .children("i")
-  //       .removeClass("fa-sort-down")
-  //       .addClass("fa-caret-right");
-  //   }
-  //   $(this).parent().next().slideToggle();
-  // });
+  
 });
